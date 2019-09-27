63aa42d50c6905cb8529cba9c726c224cd01d3d7
jeason <chenzhang7618@163.com>
2019-09-27 12:24:59 +0800

    完成增加健身页面静态功能

--- app/public/js/insertActionGroup.js
+++ app/public/js/insertActionGroup.js
@@ -3,6 +3,7 @@ window.onload = () => {
     const cloneEle = document.querySelector('.actionGroup');
     const formEle = document.querySelector('#form');
 
+    const MAXCLONENUM = 10
     function removeCloneActionItem() {
             let cloneItems = document.querySelectorAll('.cloneItem')
             console.log(cloneItems)
@@ -12,14 +13,48 @@ window.onload = () => {
             })
     }
 
+    function removeCloneItem(contentStr,cloneItemsStr) {
+        let cloneItems = document.querySelectorAll(cloneItemsStr)
+        let content = document.querySelector(contentStr)
+        console.log(cloneItems)
+        console.log(content)
+        Array.from(cloneItems).map(curr => {
+            console.log(curr)
+            content.removeChild(curr)
+        })
+    }
+
+
     ele.addEventListener("input", (e) => {
         removeCloneActionItem()
 
-        let shoutCloneNum = e.target.value;
+        let shoutCloneNum = e.target.value - 1;
+        if (shoutCloneNum > MAXCLONENUM) {
+            e.target.value = MAXCLONENUM
+            shoutCloneNum = MAXCLONENUM - 1;
+        }
         for(let i=0;i<shoutCloneNum;i++) {
             let cloneItem = cloneEle.cloneNode(true)
             cloneItem.classList.add('cloneItem')
             formEle.appendChild(cloneItem)
         }
+    })
+    document.querySelector('#resetForm').addEventListener('click', (e) => {
+        removeCloneActionItem();
+    })
+    document.querySelector('#pushMyWriteActionGroupBtn').addEventListener('click', (e) => {
+        removeCloneItem('#modalBody', '.cloneText')
+        let inputItems = formEle.elements['input'];
+        let modeEle = document.querySelector('#modalBody');
+        console.log(modeEle)
+
+        Array.from(inputItems).map(cur => {
+            console.log(cur.value)
+            let divItem = document.createElement('div');
+            divItem.classList.add('cloneText')
+            let textItem = document.createTextNode(cur.value)
+            divItem.appendChild(textItem);
+            modeEle.appendChild(divItem);
+        })
     })
 }
